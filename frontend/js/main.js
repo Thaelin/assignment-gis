15744e976bc950ad397886143580cbfc297eb12e
Thaelin <desino.SK@gmail.com>
2018-10-31 10:33:28 +0100

    Added frontend dependencies and basic frontend structure

---
+++ frontend/js/main.js
@@ -0,0 +1,8 @@
+
+ 
+mapboxgl.accessToken = 'pk.eyJ1IjoiZmxheXRydWUiLCJhIjoiY2pud3V2b2k0MDFybDNxcWw0cm13dmwwZiJ9.96Hi1GHh8GNtHaiMoRkq0w';
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v10'
+});
